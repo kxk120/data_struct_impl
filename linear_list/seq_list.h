f71978acfcd550905bd33d3909ffeb5dc098a69a
kong.xiangkun <kxk120@gmail.com>
2014-01-10 23:44:55 +0800

    add declaration of function

--- linear_list/seq_list.h
+++ linear_list/seq_list.h
@@ -5,15 +5,23 @@
  * This struct is a sequence list that max length is ELEMENT_NUM_MUX.
  *
  * Note:
- * Data element is not necessarily char type, it can be
- * any struct type.
+ * Data element is not necessarily char type, it can be any struct type.
  */
 typedef struct {
-        unsigned char *data;    /* store all data element */
+        unsigned char *data;    /* store all data element sequentially */
         int element_size;       
         int length;             /* length of list */
 } seq_list;
 
 #define ELEMENT_NUM_MAX (4096)
+
+/* Export function */
+extern bool init_list(seq_list *s, const int);
+extern int  get_list_len(seq_list *const);
+extern void cleanup_list(seq_list *const);
+extern bool insert_list_element(seq_list *const, const void *const, const int);
+extern bool delete_list_element(seq_list *const, const int);
+extern bool fetch_list_element(seq_list *const, void *, const int);
+extern void destory_list(seq_list *const);
 
 #endif /*SEQ_LIST_H_*/
